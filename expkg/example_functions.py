d5ee1c138c14fd11605aea3b952c830d4c288729
oceaquaris <rzshrote@gmail.com>
2022-01-27 14:01:39 -0500

    improve example

--- expkg/example_functions.py
+++ expkg/example_functions.py
@@ -9,6 +9,19 @@ def fn1(arg1, arg2):
     """
     Test function 1
 
+    .. math::
+        \\mathbf{Z} = \\begin{bmatrix} \\mathbf{Z_{misc}} & \\mathbf{Z_{a}} \\end{bmatrix}
+
+    .. math::
+        \\mathbf{Y} = \\mathbf{XB} + \\mathbf{ZU} + \\mathbf{E}
+
+    Section:
+
+    - :math:`\\mathbf{Z}` is a matrix.
+    - :math:`\\mathbf{Z_{misc}}` is a matrix of miscellaneous random effect predictors of shape ``(n,p_misc)``
+    - :math:`\\mathbf{Z_{a}}` is a matrix of additive genomic marker effect predictors of shape ``(n,p_a)``
+    - ``p_misc`` + ``p_a`` = ``p``.
+
     Parameters
     ----------
     arg1 : object
